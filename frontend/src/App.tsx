b2a9b435aea7b42840dc68d88987bef5e24cd42a
Dhiego Rosa <dhieeego@gmail.com>
2025-12-11 18:39:43 -0300

    Fix: Remove unused props in App.tsx to fix build error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,11 @@
-import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
-import { Callback } from './pages/Callback';
 
 function App() {
-  const [token, setToken] = useState<string>(localStorage.getItem('ml_token') || '');
-  const [userId, setUserId] = useState<string>(localStorage.getItem('ml_user_id') || '');
-
-  const setAuth = (newToken: string, newUserId: string) => {
-    localStorage.setItem('ml_token', newToken);
-    localStorage.setItem('ml_user_id', newUserId);
-    setToken(newToken);
-    setUserId(newUserId);
-  };
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home token={token} userId={userId} />} />
-        <Route path="/callback" element={<Callback setAuth={setAuth} />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
